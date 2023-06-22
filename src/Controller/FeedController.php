b536d956ae0c3b60d45e46cc7012e734f1c64432
DeveloppementWeb19 <pierre-louis.clavel@laposte.net>
2023-06-22 19:49:17 +0200

    add xml feed

--- src/Controller/FeedController.php
+++ src/Controller/FeedController.php
@@ -3,6 +3,9 @@
 namespace App\Controller;
 
 use App\Repository\ArticleRepository;
+use App\Repository\CategoryRepository;
+use App\Repository\ChampionshipRepository;
+use App\Repository\InjuryArticleRepository;
 use Eko\FeedBundle\Feed\FeedManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,18 +14,28 @@ use Symfony\Component\Routing\Annotation\Route;
 class FeedController extends AbstractController {
 
     public function __construct(public FeedManager $feedManager,
-                                public ArticleRepository $articleRepository)
+                                public ArticleRepository $articleRepository,
+                                public ChampionshipRepository $championshipRepository,
+                                public InjuryArticleRepository $injuryArticleRepository,
+                                public CategoryRepository $categoryRepository)
     {
     }
 
     #[Route('/feed', name:'app_feed')]
     public function feed():Response
     {
-        $articles = $this->articleRepository->findAll();
+        $champs = $this->championshipRepository->findChampsFromSeason(2022);
+        $activeChamps = $this->championshipRepository->findActiveChamps();
+        /*$champs = $this->championshipRepository->findActiveChamps();*/
+        $injuryArticles = $this->injuryArticleRepository->getLastInjuryArticles($champs);
+        $lastArticles = $this->articleRepository->getLastArticles();
+        $category = $this->categoryRepository->findOneBy(['slug' => 'mercato']);
+        $mercatoArticles = $this->articleRepository->getMercatoTabArticles($activeChamps, $category);
 
-        $feed = $this->feedManager->get('article');
-        $feed->addFromArray($articles);
-
-        return new Response($feed->render('rss'));
+        return $this->render('feed.txt.twig', [
+            'injury_articles' => $injuryArticles,
+            'mercato_articles' => $mercatoArticles,
+            'last_articles' => $lastArticles
+        ]);
     }
 }
