01252168f94f3294897a9e8f3eab3e0f03a25dfc
DeveloppementWeb19 <pierre-louis.clavel@laposte.net>
2023-07-14 11:59:20 +0200

    add adversting

---
+++ public/js/ads/config_sfbxapp_app_consent.js
@@ -0,0 +1,9 @@
+const configSFBXAppConsent = {
+    appKey: '95f76996-4a6c-40f8-8c38-d3598f597066',
+    privacyWidget: { // Remove this part if you don't wish to integrate the CMP privacy center button to your site
+        color: 'clear', // Theme 'clear' or 'dark'
+        position: 'bottomLeft', // Position 'bottomLeft' or 'bottomRight'
+        text: 'Privacy center', // Text of the button
+    },
+    forceGDPRApplies: false,
+}
